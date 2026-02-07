39303a74b477c492ed7e2aab1fa20d0e727e041d
oliveiradniel <kadadniel@gmail.com>
2026-02-07 17:28:47 -0300

    feat: create the product table columns using TanStack Table

---
+++ src/pages/home/use-columns.tsx
@@ -0,0 +1,80 @@
+import { useMemo } from 'react';
+
+import { cn } from '@/lib/utils';
+
+import type { ColumnDef } from '@tanstack/react-table';
+import type { Product } from '@/entities/product';
+
+export function useColumns(): ColumnDef<Product>[] {
+  return useMemo<ColumnDef<Product>[]>(
+    () => [
+      {
+        id: 'name',
+        accessorFn: ({ name }) => name,
+        header: 'PRODUTO',
+      },
+      {
+        id: 'code',
+        accessorFn: ({ code }) => code,
+        header: 'CÓDIGO',
+      },
+      {
+        id: 'description',
+        accessorFn: ({ description }) => description,
+        cell: ({ row }) => {
+          const description = row.original.description;
+
+          return <div className="max-w-sm truncate text-sm">{description}</div>;
+        },
+        header: 'DESCRIÇÃO',
+      },
+      {
+        id: 'price',
+        accessorFn: ({ price }) => price,
+        cell: ({ row }) => {
+          return new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL',
+          }).format(row.original.price);
+        },
+        header: 'PREÇO',
+      },
+      {
+        id: 'is-active',
+        accessorFn: ({ isActive }) => isActive,
+        cell: ({ row }) => {
+          const isActive = row.original.isActive;
+
+          return (
+            <div
+              className={cn(
+                'flex justify-center rounded-md px-4 py-2 text-xs font-semibold',
+                isActive
+                  ? 'bg-green-500/10 text-green-600'
+                  : 'bg-red-500/10 text-red-600',
+              )}
+            >
+              <span className="text-center text-xs">
+                {isActive ? 'Ativo' : 'Inativo'}
+              </span>
+            </div>
+          );
+        },
+        header: 'STATUS',
+      },
+      {
+        id: 'created-at',
+        accessorFn: ({ createdAt }) => createdAt,
+        cell: ({ row }) => {
+          return new Date(row.original.createdAt).toLocaleDateString('pt-BR', {
+            day: '2-digit',
+            month: '2-digit',
+            year: '2-digit',
+          });
+        },
+        header: 'DATA DE CRIAÇÃO',
+      },
+    ],
+    [],
+  );
+}
