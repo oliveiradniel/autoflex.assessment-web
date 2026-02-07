c56478481e9f8c927d4efd4aad242650b72ff3f4
oliveiradniel <kadadniel@gmail.com>
2026-02-06 23:46:20 -0300

    chore: initial setup shadcn/ui with color palette

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import path from 'node:path';
 
 export default defineConfig({
   plugins: [
@@ -12,4 +13,9 @@ export default defineConfig({
     }),
     tailwindcss(),
   ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 });
