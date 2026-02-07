4692b3a0ff145b1b904118fb42c2fe8d830a1809
oliveiradniel <kadadniel@gmail.com>
2026-02-07 12:11:09 -0300

    feat: add sidebar

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,18 +1,91 @@
-import * as React from 'react';
-
-import { Outlet, createRootRoute } from '@tanstack/react-router';
+import {
+  Link,
+  Outlet,
+  createRootRoute,
+  useLocation,
+} from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
+
+import logoSideBar from '@/assets/images/logo-sidebar.png';
+import { FactoryIcon, HomeIcon, PackageIcon } from 'lucide-react';
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarTrigger,
+} from '@/components/ui/sidebar';
+import { SidebarProvider } from '@/components/ui/sidebar/sidebar-provider';
 
 export const Route = createRootRoute({
   component: RootComponent,
 });
 
 function RootComponent() {
+  const location = useLocation();
+
+  const items = [
+    {
+      title: 'Início',
+      url: '/',
+      icon: HomeIcon,
+      hasAction: false,
+    },
+    {
+      title: 'Produtos',
+      url: '/produtos',
+      icon: PackageIcon,
+      hasAction: true,
+    },
+    {
+      title: 'Matérias Primas',
+      url: '/materias-primas',
+      icon: FactoryIcon,
+      hasAction: true,
+    },
+  ];
+
   return (
-    <React.Fragment>
-      <div>Hello "__root"!</div>
-      <Outlet />
-      <TanStackRouterDevtools />
-    </React.Fragment>
+    <SidebarProvider>
+      <Sidebar collapsible="icon" className="p-2">
+        <SidebarHeader className="group-data-[collapsible=icon]:hidden">
+          <img src={logoSideBar} alt="Logo Sidebar" />
+        </SidebarHeader>
+
+        <SidebarContent>
+          <SidebarGroup>
+            <SidebarGroupLabel>MENU PRINCIPAL</SidebarGroupLabel>
+            <SidebarMenu>
+              {items.map((item) => {
+                const isActive = item.url === location.pathname;
+
+                return (
+                  <SidebarMenuItem key={item.url}>
+                    <SidebarMenuButton isActive={isActive} asChild>
+                      <Link to={item.url} className="px-3 py-5">
+                        <item.icon />
+                        <span>{item.title}</span>
+                      </Link>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                );
+              })}
+            </SidebarMenu>
+          </SidebarGroup>
+        </SidebarContent>
+      </Sidebar>
+
+      <main>
+        <SidebarTrigger />
+
+        <Outlet />
+        <TanStackRouterDevtools />
+      </main>
+    </SidebarProvider>
   );
 }
